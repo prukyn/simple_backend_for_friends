28a028659aec66995bc1363d929c568b35639b23
Petro Rykun <rukyn.petro@gmail.com>
2019-12-26 00:19:11 +0200

    Write headers in json

--- app/helpers.py
+++ app/helpers.py
@@ -1,13 +1,15 @@
 from datetime import date
 
 def transform_to_json(headers: list, query_result: list):
-    result = []
+    result = {}
+    result['headers'] = headers
+    result['data'] = []
     for item in query_result:
         res = {}
         for header, val in zip(headers, item):
             if isinstance(val, date):
                 val = f"{val.day}-{val.month}-{val.year}"
             res[header] = val
-        result.append(res)
+        result['data'].append(res)
 
     return result
